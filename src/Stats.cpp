44fef66f376f9bdaf22443716f2c470f838f921b
Johannes de Fine Licht <johannes@musicmedia.dk>
2017-08-16 15:14:49 +0200

    Change to valid defaults and add Stats executable

---
+++ src/Stats.cpp
@@ -0,0 +1,18 @@
+#include "MatrixMatrix.h"
+
+int main(int argc, char **argv) {
+  float frequency = kFrequency;
+  if (argc > 1) {
+    frequency = std::stof(argv[1]);
+  }
+  const unsigned long long nOps =
+      2 * static_cast<unsigned long long>(kSize) * kSize * kSize;
+  std::cout << "Frequency:            " << frequency << " MHz\n";
+  std::cout << "Number of operations: " << nOps << " ("
+            << static_cast<float>(nOps) << ")\n";
+  const auto peakPerf = 2e-3 * kTileSizeN * kKernelWidth * frequency;
+  std::cout << "Peak performance:     " << peakPerf << " GOp/s\n";
+  std::cout << "Peak runtime:         " << nOps / (1e9 * peakPerf)
+            << " seconds.\n";
+  return 0;
+}
